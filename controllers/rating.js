280f8574a68330677040360fcc5f64e3ef09e77a
MotoClique <40442195+MotoClique@users.noreply.github.com>
2018-09-03 15:59:17 +0530

    regex to eq

--- controllers/rating.js
+++ controllers/rating.js
@@ -10,13 +10,13 @@ const Rating = mongoose.model('Rating');
 module.exports.getRating = function(req,res){//Fetch
 	var query = {};
 	if(req.query.rating_id){
-		query.rating_id = {"$regex":req.query.rating_id, "$options":"i"};
+		query.rating_id = {"$eq":req.query.rating_id};
 	}
 	if(req.query.service_id){
-		query.service_id = {"$regex":req.query.service_id, "$options":"i"};
+		query.service_id = {"$eq":req.query.service_id};
 	}
 	if(req.query.deleted){
-		query.deleted = {"$regex":req.query.deleted, "$options":"i"};
+		query.deleted = {"$eq":req.query.deleted};
 	}
 	else{
 		query.deleted = {"$ne": true};
